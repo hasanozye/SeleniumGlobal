d4e788c7932bf1b9fadecfe6242f213d345d553a
Hasan Ozyer <hasanozyer06@gmail.com>
2023-07-08 18:28:35 +0300

    "Added custom locators for form elements on demoqa.com"

---
+++ src/main/java/gun09/MyLocatorsClass.java
@@ -0,0 +1,14 @@
+package gun09;
+
+import org.openqa.selenium.By;
+
+public class MyLocatorsClass {
+
+
+    public static final By lName = By.cssSelector("#userName");
+    public static final By lEmail = By.id("userEmail");
+    public static final By lCAdress = By.cssSelector("#currentAddress");
+    public static final By lPAddress = By.id("permanentAddress");
+    public static final By lSubmit = By.id("submit");
+
+}
