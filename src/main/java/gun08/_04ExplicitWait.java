4865520b1956e7dcc0eb1e56c6df7bc51b16c87e
Hasan Özyer <hasanozyer06@gmail.com>
2023-07-14 17:34:01 +0300

    Implemented an explicit wait to handle the modal dialog on the DemoQA website. Clicked on the "Show Small Modal" button, waited for the modal to appear, clicked on the "Close" button, and then waited for the modal to disappear.

--- src/main/java/gun08/_04ExplicitWait.java
+++ src/main/java/gun08/_04ExplicitWait.java
@@ -14,37 +14,36 @@ public class _04ExplicitWait {
     WebDriverWait wait;
 
     @Test
-    public void dialogProcess() {
+    public void dialogProcess(){
         driver = Utils.getChromeDriver();
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.get("https://demoqa.com/modal-dialogs");
-        click(By.id("showSmallModal"));
 
 //        driver.findElement(By.id("showSmallModal")).click();
-//        click(driver,By.id("showSmallModal"));
+        click(driver,By.id("showSmallModal"));
+//        click(By.id("showSmallModal"));
+
 //        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("closeSmallModal"))).click();
-        click(driver,10,By.id("closeSmallModal"));
+        click(By.id("closeSmallModal"));
+
 
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("closeSmallModal")));
-
 
         driver.quit();
     }
 
 
-    void click(By locator) {
+    void click (By locator){
         wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
 
-    void click(WebDriver driver, By locator) {
-        click(driver, 10, locator);
+    void click (WebDriver driver, By locator){
+        click(driver,10,locator);
     }
-
-    void click(WebDriver driver, int seconds, By locator) {
+    void click (WebDriver driver, int seconds, By locator){
         new WebDriverWait(driver, Duration.ofSeconds(seconds))
                 .until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
-
 }
