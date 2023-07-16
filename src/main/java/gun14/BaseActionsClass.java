404712f8b237588436f30205b640d35af272b856
Hasan Ozyer <hasanozyer06@gmail.com>
2023-07-16 16:30:06 +0300

    Initialize the WebDriver, WebDriverWait, and JavascriptExecutor, and maximize the browser window in the BaseActionsClass, serving as a reusable base class for common actions in Selenium tests.

---
+++ src/main/java/gun14/BaseActionsClass.java
@@ -0,0 +1,28 @@
+package gun14;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseActionsClass {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    JavascriptExecutor js;
+
+
+
+    public BaseActionsClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        js = (JavascriptExecutor) driver;
+    }
+
+
+}
